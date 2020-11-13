099f0e752b0f4c4aeff42e6b15cb479b16ec9276
pangqiao <qiaowei1976@qq.com>
2020-11-13 22:27:52 +0800

    添加进度条

--- photo_handler.py
+++ photo_handler.py
@@ -10,7 +10,7 @@
 #                 according to the created time.
 #
 
-from tkinter import *
+import tkinter as tk
 import tkinter.filedialog
 import tkinter.messagebox
 import platform
@@ -26,12 +26,15 @@ class Color():
     bg = "tan"
     fg = 'Black'
 
-class Panel(object):
+supportList = ['.jpg', '.JPG', '.png', '.mp4', '.mov', '.bmp', '.heic']
+
+class photo_handler(object):
     def __init__(self, master):
         self.root = master
         self.root.title("照片分类")
+        bg = self.root.cget("background") 
 
-        self.dim_str = str(260) + 'x' + str(60)
+        self.dim_str = str(260) + 'x' + str(80)
         self.root.geometry(self.dim_str)
         self.root.resizable(0, 0)
         self.root.attributes("-alpha", 0.9)
@@ -40,30 +43,48 @@ class Panel(object):
         self.MONTH_DAY = (float)(dir_format.MONTH_DAY)
         self.MONTH = (float)(dir_format.MONTH)
         self.selected = (float)(dir_format.MONTH_DAY)
+        self.num = 0
 
     def create_panel(self):
-
         # The background of the panel.
-        label = Label(self.root, bg = Color().bg, fg = Color().fg)
-        label.place(x = 0, y = 0, width = 260, height = 60)
-
-        # The button to select the folder.
-        self.Select_button = Button(self.root, text = '选文件夹', font = ('华文楷体', 10, 'normal'),
-            bg = Color().bg, fg = Color().fg, command = lambda:self.cmd_select_dir())
-        self.Select_button.place(x = 165, y = 5, width = 90, height = 50)
+        label = tk.Label(self.root, bg = Color().bg, fg = Color().fg)
+        label.place(x = 0, y = 0, width = 260, height = 80)
 
         # The radio button to select the folder type: year + month, or year + month + day.
-        radio_info = [('20201030',  (float)(dir_format.MONTH_DAY), 0, 0),
-            ('202010  ', (float)(dir_format.MONTH), 0, 30)]
+        radio_info = [('20201030',  (float)(dir_format.MONTH_DAY), 0, 3),
+            ('202010  ', (float)(dir_format.MONTH), 0, 28)]
 
-        self.value = IntVar()
+        self.value = tk.IntVar()
         self.value.set((float)(dir_format.MONTH_DAY))
 
         for dir_string, num, pos_x, pos_y in radio_info:
-            radio = Radiobutton(self.root, text = dir_string, value = num,
+            radio = tk.Radiobutton(self.root, text = dir_string, value = num,
                 bg = Color().bg, fg = Color().fg, command = self.cmd_radio_clicked,
                 font = ('Kailti', 9, 'normal'), variable = self.value)
-            radio.place(x = pos_x, y = pos_y, width = 160, height = 30)
+            radio.place(x = pos_x, y = pos_y, width = 160, height = 25)
+
+        # The button to select the folder.
+        self.btn = tk.Button(self.root, text = '选文件夹', font = ('华文楷体', 10, 'normal'),
+            bg = Color().bg, fg = Color().fg, command = lambda:self.cmd_select_dir())
+        self.btn.place(x = 160, y = 8, width = 80, height = 40)
+
+        label = tk.Label(self.root, text = '进度：', font = ('Kailti', 8, 'normal'),
+            bg = Color().bg, fg = Color().fg).place(x = 2, y = 58, width = 75, height = 20)
+
+        # Create the backgound of progress bar
+        self.progress_width = 180;
+        self.progress_high = 10;
+        self.cv = tk.Canvas(self.root, bg = 'white')
+        self.cv.place(x = 64, y = 62, width = self.progress_width, height = self.progress_high)
+        self.fill = self.cv.create_rectangle(0, 0, 0, self.progress_high, fill = "green")
+
+    def Draw_progress(self, total, num):
+        print(total, num)
+        n = (self.progress_width / total) * num
+        self.cv.coords(self.fill, (0, 0, n, self.progress_high))
+        self.root.update()
+        if total < 20:
+            time.sleep(0.02)
 
     def cmd_radio_clicked(self):
         self.selected = (float)(self.value.get())
@@ -72,38 +93,40 @@ class Panel(object):
         try:
             folder = tkinter.filedialog.askdirectory()
             if folder == '': return
-
             if(platform.system()=='Windows'):
                 folder = '\\'.join(folder.split('/'))
 
-            handle = photo_handler(folder, self.selected)
-            handle.run()
+            self.btn['state'] = 'disable'
+            self.grep_file(folder)
+            print("归类完成！\n")
+            self.btn['state'] = 'normal'
 
         except PermissionError:
             tkinter.messagebox.showinfo('提示', '有文件在处理, 请关闭文件夹或文件！')
 
-class photo_handler(object):
-
-    supportList = ['.jpg', '.JPG', '.png', '.mp4', '.mov', '.bmp', '.heic']
-
-    def __init__(self, dir, selected):
-        self.path = dir
-        self.selected = selected
-
-    def run(self):
-        self.grep_file(self.path)
-        print("归类完成！\n")
-
     def grep_file(self, dir):
-
+        self.path = dir
+        self.total = 0;
         prev_dir = os.getcwd()
         os.chdir(dir)
 
         for root, subdirs, files in os.walk("."):
             for file in files:
                 name, ext = os.path.splitext(file)
-                if ext in self.supportList:
-                    self.copy_file(root, file)
+                if ext in supportList:
+                    self.total += 1;
+
+        self.num = 0
+        for root, subdirs, files in os.walk("."):
+            for file in files:
+                name, ext = os.path.splitext(file)
+                if ext in supportList:
+                    try:
+                        self.copy_file(root, file)
+                    except shutil.SameFileError:
+                        pass
+                    self.num += 1
+                    self.Draw_progress(self.total, self.num)
 
         os.chdir(prev_dir)
 
@@ -137,7 +160,8 @@ class photo_handler(object):
 """"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
 if __name__ == '__main__':
-    root = Tk()
+    #root = Tk()
+    root = tk.Tk()
     root.title("照片分类")
-    Panel(root)
+    photo_handler(root)
     root.mainloop()
